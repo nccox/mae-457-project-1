5d9279551f0457cedea4caec3db8c74365a32640
Nathan Cox <nccox2@ncsu.edu>
2019-04-09 21:00:43 -0400

    Added main script

---
+++ main.m
@@ -0,0 +1,15 @@
+% AVL file of aircraft
+aircraft = 'm7-260C.avl';
+
+% Velocities to test (in knots)
+velocity_knots = 43:5:158;
+% Convert velocities to meters per second
+param = velocity_knots.*0.514444;
+
+% CG location (units aft of LE)
+xcg_inches = 15;
+% Convert to meters
+xcg = xcg_inches.*0.0254;
+
+% Run the AVL demo
+AVL_demo_run(aircraft,param,xcg)
