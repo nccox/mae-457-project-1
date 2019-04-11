2637c5733cbdbea6bb9dd1cdc5509a32f9385185
Nathan Cox <nccox2@ncsu.edu>
2019-04-11 14:46:46 -0400

    Fixed bug calling demo run function instead of run function

--- main.m
+++ main.m
@@ -5,11 +5,21 @@ aircraft = 'm7-260C.avl';
 velocity_knots = 43:5:158;
 % Convert velocities to meters per second
 param = velocity_knots.*0.514444;
+%param = 33:0.1:36;
+%param = 33.5:0.005:33;
+%param = 33.555215;
 
 % CG location (units aft of LE)
 xcg_inches = 21.895428;
 % Convert to meters
 xcg = xcg_inches.*0.0254;
 
+mass = 842.00;
+
 % Run the AVL demo
-AVL_demo_run(aircraft,param,xcg)
+AVL_run(aircraft,param,xcg,mass)
+
+%% find Vtrim
+load('results.mat');
+vtrim = interp1(de,vel,0);
+fprintf('V_trim for delta_e=0: %.6f',vtrim);
