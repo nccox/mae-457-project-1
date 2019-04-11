d0977aa69b18b390ca471d25db961a075b6640e1
Nathan Cox <nccox2@ncsu.edu>
2019-04-11 13:31:23 -0400

    Change xcg location to value corresponding to 10% static margin

--- main.m
+++ main.m
@@ -7,7 +7,7 @@ velocity_knots = 43:5:158;
 param = velocity_knots.*0.514444;
 
 % CG location (units aft of LE)
-xcg_inches = 15;
+xcg_inches = 21.895428;
 % Convert to meters
 xcg = xcg_inches.*0.0254;
 
